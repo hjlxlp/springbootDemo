fd2d5d57a30e8b7ff7d5dacb4a5bb239361ae545
huangjiale <2247450823@qq.com>
2019-03-22 15:11:36 +0800

    aa

--- src/test/java/com/example/demo/DemoTest.java
+++ src/test/java/com/example/demo/DemoTest.java
@@ -4,9 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,6 +35,24 @@ public class DemoTest {
         List<String> ordIdList = Arrays.asList(str.split(","));
         Integer n = 90503;
         System.out.println(ordIdList.contains(String.valueOf(n)));
+    }
+
+    @Test
+    public void test02() throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://m.zmyouke.com/uke-api/uke-apollo/dekayExcel/12228";
+        HttpEntity<byte[]> entity = new HttpEntity<>(new HttpHeaders());
+        ResponseEntity<byte[]> res = restTemplate.exchange(url, HttpMethod.GET, entity, byte[].class);
+        OutputStream outputStream = new FileOutputStream(new File("C:/test.xlsx"), false);
+        outputStream.write(res.getBody());
+        outputStream.flush();
+        outputStream.close();
+        System.out.println(res.getBody());
+        /*int length = 0;
+        byte[] buf = new byte[1024];
+        while (inputStream.read(buf, 0, length) != -1) {
+            outputStream.write(buf,0,length);
+        }*/
     }
 
 }
