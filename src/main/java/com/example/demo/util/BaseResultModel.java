ec0e475da7c54c97ca86bf6f3bf0fbd509d19a54
huangjiale <huangjiale@aikesaisi.com>
2021-07-09 16:27:06 +0800

    test

--- src/main/java/com/example/demo/util/BaseResultModel.java
+++ src/main/java/com/example/demo/util/BaseResultModel.java
@@ -1,7 +1,5 @@
 package com.example.demo.util;
 
-import javafx.util.Pair;
-
 import java.io.Serializable;
 import java.util.Optional;
 
@@ -11,114 +9,114 @@ import java.util.Optional;
  */
 public class BaseResultModel<T> implements Serializable {
 
-    private static final int DefaultErrorCode = 500;
+	private static final int DefaultErrorCode = 500;
 
-    public BaseResultModel() {
+	public BaseResultModel() {
 
-    }
+	}
 
-    public BaseResultModel(T data) {
-        this.data = data;
-    }
-
-
-    private T data = null;
-
-    /**
-     * 状态码,0表示成功,其他表示错误,
-     * 默认为0,如果需要设置异常code,请使用setError 一起设置
-     */
-    private int code = 0;
-
-    private String msg = "";
-
-    private String errorMsg = "";
+	public BaseResultModel(T data) {
+		this.data = data;
+	}
 
 
-    public boolean getSuccess() {
-        return this.code == 0;
-    }
+	private T data = null;
 
-    public BaseResultModel<T> setError(int code, String errorMsg) {
-        this.code = code;
-        this.errorMsg = errorMsg;
-        return this;
-    }
+	/**
+	 * 状态码,0表示成功,其他表示错误,
+	 * 默认为0,如果需要设置异常code,请使用setError 一起设置
+	 */
+	private int code = 0;
 
-    public BaseResultModel<T> setError(BizException e) {
-        return setError(e.getCode(), e.getErrorMsg());
-    }
+	private String msg = "";
 
-    public BaseResultModel<T> setError(Pair<Integer, String> errorPair) {
-        return setError(errorPair.getKey(), errorPair.getValue());
-    }
-
-    public BaseResultModel<T> setMsg(String msg) {
-        this.msg = msg;
-        return this;
-
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public int getCode() {
-        return this.code;
-    }
-
-    public String getErrorMsg() {
-        return errorMsg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public BaseResultModel<T> setData(T data) {
-        this.data = data;
-        return this;
-    }
+	private String errorMsg = "";
 
 
-    public static BaseResultModel of(Pair<Integer, String> errorPair) {
-        return new BaseResultModel().setError(errorPair);
-    }
+	public boolean getSuccess() {
+		return this.code == 0;
+	}
 
-    public static BaseResultModel of(Exception e) {
-        //msg
-        StringBuilder msg = new StringBuilder();
-        msg.append("异常类型:");
-        msg.append(e.getClass().getName());
-        msg.append(",");
-        msg.append("异常信息:");
-        msg.append(e.getMessage());
-        msg.append(",");
-        msg.append("异常堆栈追踪:");
-        Optional.ofNullable(e.getStackTrace()).ifPresent(
-                (stacks) -> {
-                    for (StackTraceElement s : stacks) {
-                        msg.append(s);
-                    }
-                }
-        );
-        //code
-        int code;
-        if (e instanceof BizException) {
-            code = ((BizException) e).getCode();
-        } else {
-            code = DefaultErrorCode;
-        }
-        //construct
-        return new BaseResultModel().setError(code, msg.toString());
-    }
+	public BaseResultModel<T> setError(int code, String errorMsg) {
+		this.code = code;
+		this.errorMsg = errorMsg;
+		return this;
+	}
 
-    public static <T> BaseResultModel of(T data) {
-        return new BaseResultModel<>().setData(data);
-    }
+	public BaseResultModel<T> setError(BizException e) {
+		return setError(e.getCode(), e.getErrorMsg());
+	}
 
-    public static void main(String[] args) {
-        System.out.println(NullPointerException.class.getName());
-    }
+	public BaseResultModel<T> setError(Integer key, String value) {
+		return setError(key, value);
+	}
+
+	public BaseResultModel<T> setMsg(String msg) {
+		this.msg = msg;
+		return this;
+
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public int getCode() {
+		return this.code;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public BaseResultModel<T> setData(T data) {
+		this.data = data;
+		return this;
+	}
+
+
+	public static BaseResultModel of(Integer key, String value) {
+		return new BaseResultModel().setError(key, value);
+	}
+
+	public static BaseResultModel of(Exception e) {
+		//msg
+		StringBuilder msg = new StringBuilder();
+		msg.append("异常类型:");
+		msg.append(e.getClass().getName());
+		msg.append(",");
+		msg.append("异常信息:");
+		msg.append(e.getMessage());
+		msg.append(",");
+		msg.append("异常堆栈追踪:");
+		Optional.ofNullable(e.getStackTrace()).ifPresent(
+				(stacks) -> {
+					for (StackTraceElement s : stacks) {
+						msg.append(s);
+					}
+				}
+		);
+		//code
+		int code;
+		if (e instanceof BizException) {
+			code = ((BizException) e).getCode();
+		} else {
+			code = DefaultErrorCode;
+		}
+		//construct
+		return new BaseResultModel().setError(code, msg.toString());
+	}
+
+	public static <T> BaseResultModel of(T data) {
+		return new BaseResultModel<>().setData(data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(NullPointerException.class.getName());
+	}
 
 }
